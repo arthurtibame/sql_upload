dff09e69fc2fdc414651a6010d8881eb6b597c8e
arthurtibame <arthurtibame@gmail.com>
2020-11-04 19:35:27 +0800

    1104

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v $1:/opt/inputs arthurtibame/sql_upload:0.0.1 python3 /opt/main.py --input /opt/inputs --type $2
+docker run -it --rm -v $1:/opt/inputs -v $(pwd)/logs:/opt/logs -v $(pwd)/outputs arthurtibame/sql_upload:0.0.2 python3 /opt/main.py --input /opt/inputs --type $2
